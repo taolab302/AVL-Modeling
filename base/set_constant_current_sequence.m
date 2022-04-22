50415f2c20b70ed3bd317dc8c22ea0c425abedf9
taolab302 <56688091+taolab302@users.noreply.github.com>
2022-04-22 15:24:29 +0800

    Update set_constant_current_sequence.m

--- base/set_constant_current_sequence.m
+++ base/set_constant_current_sequence.m
@@ -1,10 +1,14 @@
-function [I, I_start, I_end] = set_constant_current_sequence(time_length, I_val)
+function [I, I_start, I_end] = set_constant_current_sequence(time_length, I_val, I_blank)
 
 if nargin < 2
     I_val = 10;
+    I_blank = 100000;
+elseif nargin < 3
+    I_blank = 100000;
 end
-I_start = 100001;
-I_end = time_length-100000;
+
+I_start = 1 + I_blank;
+I_end = time_length-I_blank;
 
 I = zeros(1, time_length);
 I(I_start:I_end) = I_val;
