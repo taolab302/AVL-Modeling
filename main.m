c467a8643a4e1df904e2cb86d5f5dbd0329d6279
taolab302 <56688091+taolab302@users.noreply.github.com>
2022-04-22 10:34:38 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,23 +1,23 @@
 % add base into MATLAB path
 addpath( genpath('base') );
 
-%% Figure 4A
+%% Figure 4a
 PlotWholeCellCurrent(650);
 
-%% Figure 4B
+%% Figure 4b
 PlotTailPeakIV(650);
 
-%% Figure 4C & S3A
+%% Figure 4c & S3a
 PlotWholeCellVoltage('wt', 7000);
 
-%% Figure 4D & S3B
+%% Figure 4d & S3b
 PlotWholeCellVoltage('exp-2(lf)', 7000);
 
-%% Figure 4E & S3C
+%% Figure 4e & S3c
 PlotWholeCellVoltage('exp-2(gf)', 7000);
 
-%% Figure S3D
-PlotWholeCellVoltage('shl-1', 7000);
+%% Figure S3d
+PlotWholeCellVoltage('shl-1', 8000);
 
-%% Figure S3E
-PlotWholeCellVoltage('exp-2;shl-1', 7000);
+%% Figure S3e
+PlotWholeCellVoltage('exp-2;shl-1', 8000);
